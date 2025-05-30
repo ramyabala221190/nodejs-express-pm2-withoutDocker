8694b50e9a407463d41db6f0828c46216ae313c5
Ramya <ramya.bala221190@gmail.com>
2025-05-30 17:18:04 +0530

    added deploy script

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -15,4 +15,24 @@ module.exports = {
     },
     node_args:`-r dotenv/config`
   },],
+  deploy : {
+    production : {
+       "key": "C:/Users/User/.ssh/id_rsa.pub",
+       "user" : "azureuser22",
+       "host" : ["4.201.137.36"],
+       "ref"  : "origin/master",
+       "repo" : "https://github.com/ramyabala221190/nodejs-express-pm2-withoutDocker",
+       "path" : "/var/prod/onlypm2/",
+       "post-deploy" : "npm install && npm install -g pm2 && npm run pm2-prod"
+    },
+    development:{
+      "user" : "azureuser22",
+      "host" : ["4.201.137.36"],
+      "ref"  : "origin/master",
+      "repo" : "https://github.com/ramyabala221190/nodejs-express-pm2-withoutDocker",
+      "path" : "/var/dev/onlypm2/",
+      "post-deploy" : "npm install && npm install -g pm2 && npm run pm2-dev"
+    }
+  }
 };
+
