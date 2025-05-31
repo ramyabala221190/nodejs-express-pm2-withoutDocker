7b57bc609000d3470f67de30220c8dd34a1b6844
Ramya <ramya.bala221190@gmail.com>
2025-05-31 17:33:36 +0530

    added pre-setup step

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -26,6 +26,7 @@ module.exports = {
        "ref"  : "origin/master",
        "repo" : "https://github.com/ramyabala221190/nodejs-express-pm2-withoutDocker",
        "path" : "/var/prod/onlypm2/",
+       "pre-setup":"sudo chmod +755 /var/ /usr/local/lib/ /usr/local/bin/",
        "post-setup":"node --version && npm --version && pm2 --version && npm install",
        "post-deploy" : "npm run pm2-prod-start"
     },
@@ -35,6 +36,7 @@ module.exports = {
       "ref"  : "origin/master",
       "repo" : "https://github.com/ramyabala221190/nodejs-express-pm2-withoutDocker",
       "path" : "/var/dev/onlypm2/",
+      "pre-setup":"sudo chmod +755 /var/ /usr/local/lib/ /usr/local/bin/",
       "post-setup":"node --version && npm --version && pm2 --version && npm install",
       "post-deploy" : "npm run pm2-dev-start"
     }
